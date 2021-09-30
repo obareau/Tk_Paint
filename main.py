8afeb71d528c0504dec27e67449bd7a71958827b
obareau <olivier.bareau@gmail.com>
2021-09-30 17:15:26 +0200

    grid works

--- main.py
+++ main.py
@@ -7,12 +7,30 @@ class PixelApp:
         self.root.title("Pixel Art")
         
         cell_length = 50
+        grid_width = 20
+        grid_height = 10
         
         self.drawing_grid = Canvas(self.root)    
         self.drawing_grid.grid(column=0, row=0, sticky=(N, E, S, W))
         
-        cell = Frame(self.drawing_grid, width=cell_length, height=cell_length, bg="white", highlightbackground="black", hilightcolor="black", highlightthickness="1")
-   
+        self.cells = []
+        for i in range (0, grid_height):
+            for j in range (0, grid_width):
+                cell = Frame(self.drawing_grid, width=cell_length, 
+                     height=cell_length, 
+                     bg="white", 
+                     highlightbackground="black", 
+                     highlightcolor="black", 
+                     highlightthickness="1"
+                     )
+                cell.grid(column=j, row=i,)
+                cell.bind("<Button-1>", self.tap_cell)
+                self.cells.append(cell)
+        
+        
+    def tap_cell(self, event):
+        print("Cell Taped")
+       
         
 root = Tk()
 PixelApp(root)
