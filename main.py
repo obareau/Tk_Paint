9b9bc6c6b05c9e09aaef1252f7bc94837fdd7e13
obareau <olivier.bareau@gmail.com>
2021-09-30 16:30:43 +0200

    starting blocks

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+class PixelApp:
+    
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Pixel Art")
+        
+        
+root = Tk()
+PixelApp(root)
+root.mainloop()        
