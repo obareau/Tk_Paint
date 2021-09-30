ccfe833820b68834a199799cac807dbe051953a8
obareau <olivier.bareau@gmail.com>
2021-09-30 16:45:54 +0200

    grid

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ class PixelApp:
         self.root = root
         self.root.title("Pixel Art")
         
+        cell_length = 50
+        
+        self.drawing_grid = Canvas(self.root)    
+        self.drawing_grid.grid(column=0, row=0, sticky=(N, E, S, W))
+        
+        cell = Frame(self.drawing_grid, width=cell_length, height=cell_length, bg="white", highlightbackground="black", hilightcolor="black", highlightthickness="1")
+   
         
 root = Tk()
 PixelApp(root)
